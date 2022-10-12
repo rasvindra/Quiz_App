8b7676d308457816f059afc0d4ca237909691bd8
Ravi Nagi <rasvindra@hotmail.com>
2022-10-12 14:34:28 -0700

    created HTML and JS file. added h1 and button. added array of question blocks to js

---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+var time = 45
+var timeLeft = 45
+var currentScore = ""
+
+
+var allQuestions = [
+
+{
+    question:
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+
+},
+{
+    question:
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    
+},
+{
+    question:
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    
+},
+{
+    question:
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    
+},
+{
+    question:
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    
+},
+
+
+
+]
+
+
+
