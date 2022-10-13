ad143ace882ffe3232126d44da975e1bd6cea711
Ravi Nagi <rasvindra@hotmail.com>
2022-10-13 11:08:09 -0700

    added HTML setup for quiz and structured block array for questions in javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,57 +6,55 @@ var currentScore = ""
 var allQuestions = [
 
 {
-    question:
-    answers: {
-        a:
-        b:
-        c:
-        d:
+    question: "Question 1",
+    answers: { 
+        a: "Answer1.1",
+        b: "Answer1.2",
+        c: "Answer1.3",
+        d: "Answer1.4",
     },
-
+    correctAnswer: "Answer1.3"
 },
 {
-    question:
+    question: "Question 2",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a: "Answer2.1",
+        b: "Answer2.2",
+        c: "Answer2.3",
+        d: "Answer2.4",
     },
-    
+    correctAnswer: "Answer2.1"
 },
 {
-    question:
+    question: "Question 3",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a: "Answer3.1",
+        b: "Answer3.2",
+        c: "Answer3.3",
+        d: "Answer3.4",
     },
-    
+    correctAnswer: "Answer3.2"
 },
 {
-    question:
+    question: "Question 4",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a: "Answer4.1",
+        b: "Answer4.2",
+        c: "Answer4.3",
+        d: "Answer4.4",
     },
-    
+    correctAnswer: "Answer4.4"
 },
 {
-    question:
+    question: "Question 5",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a: "Answer5.1",
+        b: "Answer5.2",
+        c: "Answer5.3",
+        d: "Answer5.4",
     },
-    
+    correctAnswer: "Answer5.2"
 },
-
-
 
 ]
 
